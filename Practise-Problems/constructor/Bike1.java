d4faaef42a4593ce3c522627e12ea885aa6d96b1
Aravind S <91778740+Arvnd147@users.noreply.github.com>
2023-01-04 21:42:19 +0530

    Add files via upload

---
+++ Practise-Problems/constructor/Bike1.java
@@ -0,0 +1,9 @@
+class Bike1{
+    Bike1()
+    {
+        System.out.println("Bike is created");
+    }
+    public static void main(String[] args) {
+        new Bike1();
+    }
+}
---
+++ Practise-Problems/constructor/Student3.java
@@ -0,0 +1,13 @@
+public class Student3 {
+    //default constructor
+    int id;
+    String name;
+
+    Student3(int id, String name)
+    {
+        System.out.println(id+" "+name);
+    }
+    public static void main(String[] args) {
+            new Student3(229,"Aravind");
+        }
+}
